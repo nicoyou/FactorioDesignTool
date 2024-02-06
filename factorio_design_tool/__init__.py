4199dcbfc9cec4eff8c625cf532cf3b0b9bd0287
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2024-02-06 15:19:30 +0900

    add: python の実行環境を整備

---
+++ factorio_design_tool/__init__.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+
+__copyright__: Final[str] = "Copyright (C) 2024 Nicoyou"
+__version__: Final[str] = "1.0.0"
+__license__: Final[str] = "MIT"
+__author__: Final[str] = "nicoyou"
+__author_email__: Final[str] = "nicoyou.info@gmail.com"
+__url__: Final[str] = "https://github.com/nicoyou/FactorioDesignTool"
